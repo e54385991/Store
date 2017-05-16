a008c0b97983d3d23db174485123debb763be874
CCF\e54385991 <54385991@qq.com>
2017-05-17 05:49:17 +0800

    update chat-processor

--- addons/sourcemod/scripting/store.bat
+++ addons/sourcemod/scripting/store.bat
@@ -1,5 +1,7 @@
 spcomp store.sp -o../plugins/store.smx
 spcomp store-trade.sp -o../plugins/store-trade.smx
 spcomp thirdperson.sp -o../plugins/thirdperson.smx
-spcomp thirdperson.sp -o../plugins/n_arms_fix.smx
+spcomp n_arms_fix.sp -o../plugins/n_arms_fix.smx
+spcomp chat-processor.sp -o../plugins/chat-processor.smx
+
 pause
